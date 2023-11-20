c14d34932d8a5fc78488468752f43e5ac7d294d4
yarivtach <53392928+yarivtach@users.noreply.github.com>
2023-11-20 17:48:18 +0200

    main.py

--- find_slope_LinReg/main.py
+++ find_slope_LinReg/main.py
@@ -2,6 +2,34 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
+
+
+
+# hm -> how mach to now how many data point to be in the dataset
+# variance -> how varible do wew want the dataset to be
+# step -> how far on avg to step up for point
+# corralition -> it to nevigate the correlation ups and downs to know the importance of data
+def create_dataset(hm, variance, step = 2, correlation=False):
+
+    val = 1 # the value of correlation
+    ys = [] # the grade vector
+
+    # we want to know if the data is a good data so we check the correlation between the variables
+    # we go over the number of the point in the dataset
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val+=step
+        elif correlation and correlation == 'neg':
+            val-=step
+
+    # orginize the dataset to put in graph
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float64), np,array(ys, dtype =np.float64)
+
 
 def find_best_slope_and_intercept(xs, ys):
     # m = (x * y - xy)/((x)^2 - (x^2))
@@ -31,12 +59,23 @@ def coefficient_of_determinition(ys_org, ys_line):
 
 
 
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     style.use('fivethirtyeight')
-    # example data
-    xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-    ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+    # # example data
+    # xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+    # ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+
+    # create a data set
+    # changing the variance might gives diffrent result -> it can automaticly calculates the determiniton of dataset
+    #   -----it is like a unit test------
+    xs, ys = create_dataset(40,40,2,correlation='pos')
+    # IF WE GET VERY LOW R-SQUARED THE DATA POSSIBLY NOT LINEAR, MAY TRY OTHER CLASSIFICTION
+
+
     m,b = find_best_slope_and_intercept(xs, ys)
 
     # create the vector Y depends on every x value
